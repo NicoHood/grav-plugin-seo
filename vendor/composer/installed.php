f1cef48d41c28a877f5246811cfe22361cd94567
NicoHood <git@nicohood.de>
2020-11-08 16:04:22 +0100

    Added composer autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e73a724107feb1ef4b46dadaff4ac4e44055da91',
+    'name' => 'nicohood/seo',
+  ),
+  'versions' => 
+  array (
+    'nicohood/seo' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e73a724107feb1ef4b46dadaff4ac4e44055da91',
+    ),
+  ),
+);
