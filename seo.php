34056a0158d465326a08160fbe453a7b46157db3
NicoHood <git@nicohood.de>
2020-11-09 18:46:19 +0100

    Added code for adding json-ld based on page frontmatter

--- seo.php
+++ seo.php
@@ -3,6 +3,7 @@ namespace Grav\Plugin;
 
 use Composer\Autoload\ClassLoader;
 use Grav\Common\Plugin;
+use Grav\Common\Data\Data;
 
 /**
  * Class SeoPlugin
@@ -52,7 +53,64 @@ class SeoPlugin extends Plugin
 
         // Enable the main events we are interested in
         $this->enable([
-            // Put your main events here
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
         ]);
+    }
+
+    /**
+     * Always add those twig variables and/or assets
+     */
+    public function onTwigSiteVariables()
+    {
+        $locator = $this->grav['locator'];
+        $page = $this->grav['page'];
+        $config = $this->mergeConfig($page);
+        $header = new Data((array)$page->header());
+        $structured_data = $header->get('structured_data', []);
+
+        if (!$config->get('active', true)) {
+            return;
+        }
+
+        // Add all template path as schema for locator
+        // TODO temporary workaround until https://github.com/getgrav/grav/pull/3059 is merged.
+        $locator->addPath('templates', '', $this->grav['twig']->twig_paths);
+
+        // General default setting
+        $add_json_ld = $config->get('add_json_ld', true);
+
+        foreach ($structured_data as $key => $data) {
+            // Check if a template exists for the given data
+            $template = 'structured-data/json-ld/' . $key . '/' . $key . '.json.twig';
+            if ($locator->findResource('templates://' . $template, true, false) === false) {
+                continue;
+            }
+
+            // The settings can be defined in the plugin or on a per-page basis.
+            $settings = new Data($config->get($key, []));
+
+            // Check if json_ld should be added to page
+            if ($settings->get('add_json_ld', $add_json_ld)) {
+                // Generate json-ld
+                $json_ld = $this->grav['twig']->processTemplate(
+                  $template,
+                  [
+                    'data' => $data,
+                    'page' => $page,
+                    'settings' => $settings
+                  ]);
+
+                $this->grav['assets']->addInlineJs($json_ld, ['type' => 'application/ld+json']);
+            }
+        }
+    }
+
+    /**
+     * Add templates directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
     }
 }
